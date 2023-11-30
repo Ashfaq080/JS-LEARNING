30b78b70a014a00358b3723b6c9e4938362dd861
Ashfaq080 <145708932+Ashfaq080@users.noreply.github.com>
2023-11-30 07:04:12 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use Strict"; //treact all JS code as newer version.
+
+// alert(3+3);   //alert is not defined in nodejs, but is define in browser.
+
+
+/*
+There are mainiy two types of dataTypes
+1.Premitive dataTypes :- 1.number :- 2 to power of 53
+                         2.bigint :- big interger memory
+                         3.string :- " "
+                         4.boolean :- true/false
+                         5.null :- standalone value (typeof null is object)
+                         6.undefined :- value not assign
+                         7.symbol :- use to fine uniqness
+
+2. Objet dataTypes => 1. an object :- any things written in curly braces {}
+                      2. an array  :- an array is a special variable, which can hold more than one value
+                      3. date      :- Date objects are created with the new Date() constructor.               
+
+
+*/
+
+
+
+let info;           //undefined
+let name ="Ashfaq";  //string
+let age = 24;       //Number
+let isSignedIn = false  //Bopolean
+
+console.table( typeof null);  //typeof keyword used to know the data type of the varaible.
