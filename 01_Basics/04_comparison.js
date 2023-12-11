18f4cddec2710fcd8bf8f062e505fe887225d347
Ashfaq080 <145708932+Ashfaq080@users.noreply.github.com>
2023-12-11 11:53:59 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,24 @@
+ /*
+
+ comparison and logical operators are used to check/ test for true and false 
+
+
+ */
+
+ let x = 5;
+
+//  console.log(8==x);     //false
+//  console.log(x==5);     //true
+//  console.log(x==="5");  // false
+//  console.log(x>= 5);    // true
+//  console.log(x<=5);     //true
+//  console.log(x > 5);    //false
+//  console.log(x < 5);    //false
+
+ console.log(null > 0);     //false
+ console.log(null == 0);    //false
+ console.log(null <= 0);    //true
+
+
+
+ // strit check (===)  it will check the value as well as its data type 
