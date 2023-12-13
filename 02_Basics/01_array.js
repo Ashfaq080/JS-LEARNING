4a6244cbaf60e3e9e6d74188593b459f6936029d
Ashfaq080 <145708932+Ashfaq080@users.noreply.github.com>
2023-12-13 14:07:46 +0000

    array

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,53 @@
+/* 
+Arrays are one of the main imp topic in JS.
+Arrays are the used to store a collection of multiple items in a single variable.
+
+Array indexes start with 0.
+[0] is the first element. [1] is the second element.
+*/
+
+//Array declaration
+
+const arrOne = [1, 2, 3, "Ashfaq", 4, "A", ]
+// console.log( typeof arrOne);
+
+const arrTwo = new Array(0,1,2,3,4)
+// console.log(typeof arrTwo);
+
+
+//changing an Array Element
+const distName = ["Bidar", "Bengaluru", "Udpi", "Kalburgi"];
+distName[0]= "vijayapur";
+// console.log(distName);
+
+
+// converting an array to a String
+const fruits = ["Apple", "Banana", "Orange", "Mango"]
+// console.log(fruits.toString());
+
+
+/* Methods in Array
+Array length
+Array toString()
+Array pop()
+Array push()
+Array shift()
+Array unshift()
+Array join()
+Array delete()
+Array concat()
+Array flat()
+Array splice()
+Array slice()
+*/
+
+
+
+
+
+
+
+
+
+
+
