f6ff260b0dd0744cdadda6b0db470fad40deccd6
Ashfaq080 <145708932+Ashfaq080@users.noreply.github.com>
2023-12-21 09:24:01 +0000

    switch statement

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,69 @@
+/*
+Switch statement :-
+The switch statement is used to perform different actions based on different conditions.
+Use the switch statement to select one of many code blocks to be executed.
+*/
+
+// SYNTAX
+// switch (key) {
+//     case value1:
+//         // code here
+//         break;
+//     case value2:
+//         // code here
+//         break;
+//     case value3:
+//         // code here
+//         break;
+
+//     default:
+//         break;
+// }
+
+const months = 4;
+
+switch (months) {            // in this you can check for ant type of data such as number, string, etc...
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("Octuber");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        break;
+}
+
+
+
+
