d598a8f8991b5f3bf0215fa517f8a1e28d922c95
Ashfaq080 <145708932+Ashfaq080@users.noreply.github.com>
2023-12-21 13:52:11 +0000

    while do while loop

---
+++ 05_iterations/02_while_DoWhile.js
@@ -0,0 +1,23 @@
+// WHILE LOOP
+let index = 0
+
+while (index <= 10) {
+    // console.log(`valu of index : ${index}`);
+    index += 2
+}
+
+let myArray = ['RAM', 'SHAM', 'RAJU', 'BABU BHIYA']
+let arr = 0;
+while (arr<myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr ++;
+}
+
+
+//  DO WHILE LOOP
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score ++
+    
+} while (score<= 10);
