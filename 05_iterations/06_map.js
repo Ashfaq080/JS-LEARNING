39dbcb61f6d5a0fd3751709ba43edfd379bf8525
Ashfaq080 <145708932+Ashfaq080@users.noreply.github.com>
2023-12-23 07:13:48 +0000

    Maps

---
+++ 05_iterations/06_map.js
@@ -0,0 +1,20 @@
+const codeingLang = ['ja', 'java', 'python', 'php', 'ruby']
+// what if we store it in a variable
+const myLoop = codeingLang.forEach((items)=>{
+    // console.log(items);
+    return items
+})
+console.log(myLoop);  // this gives undefined, as we know that when we store a function in  variable then it compulsry to return the value, but 
+
+
+
+
+
+
+
+
+
+
+
+
+
